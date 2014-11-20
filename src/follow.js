d20bcdf7c89239db064dab79fa57e34c28ace06f
tonussi <lptonussi@gmail.com>
2014-11-20 21:23:55 -0200

    Add follow

---
+++ src/follow.js
@@ -0,0 +1,16 @@
+/**
+ * TODO
+ *
+ * Follow algorithm
+ */
+
+var Follow = function (gramatica) { };
+
+Follow.prototype = function (gramatica) {
+  var map = Array.prototype.map;
+};
+
+if (require.main === module) {
+  var follow = new Follow();
+  console.log(follow);
+}
