1f0665e1d0e577e8d7c3784169ce04505b10168f
tonussi <lptonussi@gmail.com>
2014-11-20 22:06:56 -0200

    Adiciona basicos de uma glc

--- src/glc.js
+++ src/glc.js
@@ -1,17 +1,49 @@
 /**
  * TODO
  *
- * Glc algorithm
+ * GLC algorithm
  */
 
-var Glc = function (gramatica) { };
+var GLC = function (gramatica) {
 
-Glc.prototype = function (gramatica) {
   var map = Array.prototype.map;
+
 };
 
+GLC.prototype = function (gramatica) {
+
+  adiciona: {
+
+    return 1;
+
+  },
+
+  remove : {
+
+    return null;
+
+  },
+
+  busca : {
+
+    return 0;
+
+  },
+
+  mostra : {
+
+    return '';
+
+  }
+
+
+};
+
+exports.GLC = GLC;
+
 if (require.main === module) {
-  var gramatica = glc.Glc();
+  var glc = require('./glc.js');
+  var gramatica = new glc.GLC();
   glc.adiciona('S', 'aDa|bDF|cC|F');
   glc.adiciona('D', 'aaca|dD|cC');
   glc.adiciona('F', 'adac|F|cC');
